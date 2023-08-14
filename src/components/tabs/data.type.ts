7b0b302522303e5ae632e76ecaa6600429fa23ef
ravi-cybersify <ravi@cybersify.tech>
2023-08-14 17:30:08 +0530

    fix : co located products type

--- src/components/tabs/data.type.ts
+++ src/components/tabs/data.type.ts
@@ -11,30 +11,54 @@ export interface Addon {
 export interface IProduct {
   id: number;
   title: string;
-  // body_html: string;
-  // vendor: Vendor;
-  // product_type: ProductType;
-  // created_at: Date;
-  // handle: string;
-  // updated_at: Date;
-  // published_at: Date | null;
-  // template_suffix: null;
-  // status: Status;
-  // published_scope: PublishedScope;
-  // tags: string;
-  // admin_graphql_api_id: string;
-  // options: Option[];
-  // images: Image[];
+  body_html: string;
+  vendor: Vendor;
+  product_type: ProductType;
+  created_at: Date;
+  handle: string;
+  updated_at: Date;
+  published_at: Date | null;
+  template_suffix: null;
+  status: Status;
+  published_scope: PublishedScope;
+  tags: string;
+  admin_graphql_api_id: string;
+  options: Option[];
+  images: Image[];
   image: Image;
   shape: "circle" | "dot";
-  // variants?: {
-  //   [position: string]: {
-  //     image: string;
-  //   };
-  // };
+  variants: {
+    id: number;
+    product_id: number;
+    title: string;
+    price: string;
+    sku: string;
+    position: number;
+    inventory_policy: string;
+    compare_at_price: string;
+    fulfillment_service: string;
+    inventory_management: string;
+    option1: string;
+    option2: null;
+    option3: null;
+    created_at: Date;
+    updated_at: Date;
+    taxable: boolean;
+    barcode: string;
+    grams: number;
+    image_id: number;
+    weight: number;
+    weight_unit: string;
+    inventory_item_id: number;
+    inventory_quantity: number;
+    old_inventory_quantity: number;
+    requires_shipping: boolean;
+    admin_graphql_api_id: string;
+    mainImage: string;
+    imagesAll: ImagesAll;
+  }[];
 }
-
-export interface Image {
+interface Image {
   // id: number;
   // product_id: number;
   // position: number;
@@ -47,8 +71,15 @@ export interface Image {
   // variant_ids: number[];
   // admin_graphql_api_id: string;
 }
-
-export interface Option {
+interface ImagesAll {
+  A: string;
+  B: string;
+  C: string;
+  D: string;
+  E: string;
+  F: string;
+}
+interface Option {
   id: number;
   product_id: number;
   name: Name;
